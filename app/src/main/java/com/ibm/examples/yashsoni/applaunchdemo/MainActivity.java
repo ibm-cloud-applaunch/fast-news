e4689efc429b5da468d83c06ec9968da0ae826d1
Surbhi <sbhattar92@in.ibm.com>
2018-01-22 12:47:40 +0530

    Removing AppLaunch Config

--- app/src/main/java/com/ibm/examples/yashsoni/applaunchdemo/MainActivity.java
+++ app/src/main/java/com/ibm/examples/yashsoni/applaunchdemo/MainActivity.java
@@ -1,19 +1,12 @@
 package com.ibm.examples.yashsoni.applaunchdemo;
 
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.ibm.examples.yashsoni.applaunchdemo.constants.AppLaunchConstants;
 import com.ibm.mobile.applaunch.android.AppLaunchFailResponse;
 import com.ibm.mobile.applaunch.android.AppLaunchResponse;
-import com.ibm.mobile.applaunch.android.api.AppLaunch;
-import com.ibm.mobile.applaunch.android.api.AppLaunchConfig;
 import com.ibm.mobile.applaunch.android.api.AppLaunchListener;
-import com.ibm.mobile.applaunch.android.api.AppLaunchUser;
-import com.ibm.mobile.applaunch.android.api.ICRegion;
-import com.ibm.mobile.applaunch.android.api.RefreshPolicy;
 
 public class MainActivity extends AppCompatActivity implements AppLaunchListener {
 
@@ -28,11 +21,6 @@ public class MainActivity extends AppCompatActivity implements AppLaunchListener
     @Override
     protected void onResume() {
         super.onResume();
-
-        String deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-        AppLaunchConfig appLaunchConfig = new AppLaunchConfig.Builder().eventFlushInterval(10).cacheExpiration(10).fetchPolicy(RefreshPolicy.REFRESH_ON_EVERY_START).deviceId(deviceId).build();
-        AppLaunchUser appLaunchUser = new AppLaunchUser.Builder().userId("yash").custom("test", "newtest").build();
-        AppLaunch.getInstance().init(getApplication(), ICRegion.US_SOUTH_STAGING, AppLaunchConstants.appGuid, AppLaunchConstants.clientSecret, appLaunchConfig, appLaunchUser, this);
     }
 
     @Override
