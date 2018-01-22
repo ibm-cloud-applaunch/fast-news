18f08f6cb860117b89c2c85124675b94b23e70a6
Surbhi <sbhattar92@in.ibm.com>
2018-01-22 17:33:15 +0530

    Subscription page

--- app/src/main/java/com/ibm/examples/yashsoni/applaunchdemo/SubscriptionActivity.java
+++ app/src/main/java/com/ibm/examples/yashsoni/applaunchdemo/SubscriptionActivity.java
@@ -1,6 +1,8 @@
 package com.ibm.examples.yashsoni.applaunchdemo;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -8,14 +10,19 @@ import com.ibm.mobile.applaunch.android.AppLaunchFailResponse;
 import com.ibm.mobile.applaunch.android.AppLaunchResponse;
 import com.ibm.mobile.applaunch.android.api.AppLaunchListener;
 
-public class MainActivity extends AppCompatActivity implements AppLaunchListener {
+public class SubscriptionActivity extends AppCompatActivity implements AppLaunchListener {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String TAG = SubscriptionActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_subscription);
+
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar);
+        Toolbar toolbar = appBarLayout.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Hello");
     }
 
     @Override
